1fc60b65741d57d9717744f292c506d35673e2d8
Ihtisham <ahsaanullah174@gmail.com>
2022-07-18 17:42:43 +0500

    Adding Variables

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,3 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0;
 
+
+contract Token {
+    string public name; // Holds the name of the token
+    string public symbol; // Holds the symbol of the token
+    uint8 public decimals; // Holds the decimal places of the token
+    uint256 public totalSupply; // Holds the total suppy of the token
+    address payable public owner; // Holds the owner of the token
